01f6483167b53c7dc72ddbf9153b8ff59c959cc3
humayra akter <humayraakter25@gmail.com>
2023-09-15 23:17:24 +0600

    babysitter register

--- src/Components/Login/Register.js
+++ src/Components/Login/Register.js
@@ -1,13 +1,88 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import banner4 from "../../images/bg.jpg";
+import { MultiSelect } from "react-multi-select-component"; // Use curly braces and specify the named export 'MultiSelect'
 
 const Register = () => {
-  const [selectedRole, setSelectedRole] = useState(""); // State to track the selected role
+  const [selectedRole, setSelectedRole] = useState("");
+  const [selectedExperience, setSelectedExperience] = useState([]);
+  const [selectedAgeGroupPreferences, setSelectedAgeGroupPreferences] =
+    useState([]);
+  const [selectedLanguages, setSelectedLanguages] = useState([]);
+  const [selectedCertifications, setSelectedCertifications] = useState([]);
+  const [selectedAvailability, setSelectedAvailability] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState([]);
 
   const handleRoleChange = (e) => {
-    setSelectedRole(e.target.value); // Update the selected role when the dropdown value changes
+    setSelectedRole(e.target.value);
   };
+
+  const handleExperienceChange = (selectedOptions) => {
+    setSelectedExperience(selectedOptions);
+  };
+
+  const handleLocation = (selectedOptions) => {
+    setSelectedLocation(selectedOptions);
+  };
+
+  const handleCertifications = (selectedOptions) => {
+    setSelectedCertifications(selectedOptions);
+  };
+
+  const handleAgeGroupPreferencesChange = (selectedOptions) => {
+    setSelectedAgeGroupPreferences(selectedOptions);
+  };
+
+  const handleAvailability = (selectedOptions) => {
+    setSelectedAvailability(selectedOptions);
+  };
+
+  const handleLanguageChange = (selected) => {
+    setSelectedLanguages(selected);
+  };
+
+  const languageOptions = [
+    { label: "English", value: "English" },
+    { label: "Bengali", value: "Bengali" },
+  ];
+
+  const locationOptions = [
+    {
+      label: "dhanmondi",
+      value: "dhanmondi",
+    },
+    { label: "mirpur", value: "mirpur" },
+    { label: "savar", value: "savar" },
+    { label: "uttora", value: "uttora" },
+    { label: "gulshan", value: "gulshan" },
+    { label: "mohammadpur", value: "mohammadpur" },
+    { label: "banani", value: "banani" },
+    { label: "motijheel", value: "motijheel" },
+  ];
+
+  const availabilityOptions = [
+    { label: "morning", value: "morning" },
+    { label: "afternoon", value: "afternoon" },
+    { label: "evening", value: "evening" },
+  ];
+
+  const certificationOptions = [
+    { label: "CPR", value: "CPR" },
+    { label: "First Aid", value: "First Aid" },
+  ];
+
+  const experienceOptions = [
+    { value: 6, label: "6 months" },
+    { value: 12, label: "12 months" },
+    { value: 24, label: "24 months" },
+  ];
+
+  const ageGroupPreferencesOptions = [
+    { value: "infants", label: "Infants" },
+    { value: "toddlers", label: "Toddlers" },
+    { value: "school-age", label: "School-age" },
+    // Add more options as needed
+  ];
 
   return (
     <div className=" py-16">
@@ -221,42 +296,177 @@ const Register = () => {
               )}
               {selectedRole === "Babysitter" && (
                 <div>
+                  {/* Education field */}
                   <div className="form-control w-full">
                     <label className="label">
-                      <span className="label-text text-blue-700 font-bold text-md">
-                        Time Slot
+                      <span className="label-text text-left text-blue-700 font-bold text-xs">
+                        Education
                       </span>
                     </label>
-                    <input
-                      type="text"
-                      placeholder="Preferred time slot"
-                      className="input input-bordered w-full "
-                      required
-                    />
+                    <div className="input text-left w-full ">
+                      <select className="select" required>
+                        <option disabled selected>
+                          Select your education
+                        </option>
+                        <option value="male">SSC pass</option>
+                        <option value="female">HSC pass</option>
+                        <option value="other">BSc pass</option>
+                      </select>
+                    </div>
                   </div>
+                  {/* Experience dropdown */}
                   <div className="form-control w-full">
                     <label className="label">
                       <span className="label-text text-blue-700 font-bold text-md">
                         Experience
                       </span>
                     </label>
-                    <input
-                      type="text"
-                      placeholder="Experience in month"
-                      className="input input-bordered w-full "
-                      required
+                    <MultiSelect
+                      options={experienceOptions}
+                      value={selectedExperience}
+                      onChange={handleExperienceChange}
+                      labelledBy={"Select"}
+                      overrideStrings={{
+                        selectSomeItems: "Select experiences",
+                      }}
                     />
                   </div>
+                  {/* Age group preferences */}
                   <div className="form-control w-full">
                     <label className="label">
                       <span className="label-text text-blue-700 font-bold text-md">
-                        Age group
+                        Age group preferences
+                      </span>
+                    </label>
+                    <MultiSelect
+                      options={ageGroupPreferencesOptions}
+                      value={selectedAgeGroupPreferences}
+                      onChange={handleAgeGroupPreferencesChange}
+                      labelledBy={"Select"}
+                      overrideStrings={{ selectSomeItems: "Select age groups" }}
+                    />
+                  </div>
+
+                  {/* Language Spoken */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text text-blue-700 font-bold text-md">
+                        Languages Spoken
+                      </span>
+                    </label>
+                    <MultiSelect
+                      options={languageOptions}
+                      value={selectedLanguages}
+                      onChange={handleLanguageChange}
+                      labelledBy={"Select language"}
+                      overrideStrings={{ selectSomeItems: "Select language" }}
+                      disableSearch={false}
+                    />
+                  </div>
+                  {/* certifications */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text text-blue-700 font-bold text-md">
+                        Certifications
+                      </span>
+                    </label>
+                    <MultiSelect
+                      options={certificationOptions}
+                      value={selectedCertifications}
+                      onChange={handleCertifications}
+                      labelledBy={"Select certifications"}
+                      overrideStrings={{
+                        selectSomeItems: "Select certifications",
+                      }}
+                    />
+                  </div>
+                  {/* availability */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text text-blue-700 font-bold text-md">
+                        Availability
+                      </span>
+                    </label>
+                    <MultiSelect
+                      options={availabilityOptions}
+                      value={selectedAvailability}
+                      onChange={handleAvailability}
+                      labelledBy={"Select availability"}
+                      overrideStrings={{
+                        selectSomeItems: "Select availability",
+                      }}
+                    />
+                  </div>
+                  {/* location */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text text-blue-700 font-bold text-md">
+                        Location
+                      </span>
+                    </label>
+                    <MultiSelect
+                      options={locationOptions}
+                      value={selectedLocation}
+                      onChange={handleLocation}
+                      labelledBy={"Select location"}
+                      overrideStrings={{
+                        selectSomeItems: "Select preferred location",
+                      }}
+                    />
+                  </div>
+                  {/* special_skills */}
+                  <div className="form-control pt-5 w-full">
+                    <label className="label">
+                      <span className="label-text text-blue-700 font-bold text-md">
+                        special_skills
                       </span>
                     </label>
                     <input
                       type="text"
-                      placeholder="Preferred age group"
-                      className="input input-bordered w-full "
+                      placeholder="Your location"
+                      className="input input-bordered w-full"
+                      required
+                    />
+                  </div>
+                  {/* nid_no */}
+                  <div className="form-control pt-5 w-full">
+                    <label className="label">
+                      <span className="label-text text-blue-700 font-bold text-md">
+                        NID_no
+                      </span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Your nid_no"
+                      className="input input-bordered w-full"
+                      required
+                    />
+                  </div>
+                  {/* address */}
+                  <div className="form-control pt-5 w-full">
+                    <label className="label">
+                      <span className="label-text text-blue-700 font-bold text-md">
+                        Address
+                      </span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Your address"
+                      className="input input-bordered w-full"
+                      required
+                    />
+                  </div>
+                  {/* dob */}
+                  <div className="form-control pt-5 w-full">
+                    <label className="label">
+                      <span className="label-text text-blue-700 font-bold text-md">
+                        Date of Birth
+                      </span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Your dob"
+                      className="input input-bordered w-full"
                       required
                     />
                   </div>
