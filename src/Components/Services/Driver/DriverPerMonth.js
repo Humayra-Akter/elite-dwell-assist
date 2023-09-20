ad589d19cc2321801e1ec11929ac313668dea894
humayra akter <humayraakter25@gmail.com>
2023-09-21 00:29:14 +0600

    driver month added

--- src/Components/Services/Driver/DriverPerMonth.js
+++ src/Components/Services/Driver/DriverPerMonth.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import BookingDriver from "./BookingDriver";
+import DriverPerMonthCard from "./DriverPerMonthCard";
 
 const DriverPerMonth = () => {
-  return <div></div>;
+  const [drivers, setDrivers] = useState([]);
+  const [bookDriver, setBookDriver] = useState([]);
+
+  useEffect(() => {
+    fetch("service.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const driverData = data.driver;
+        setDrivers(driverData);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1
+        className="text-2xl pt-20 text-center font-black text-purple-950  px-7"
+        style={{ fontFamily: "algerian" }}
+      >
+        driver
+      </h1>
+      <div className="grid grid-cols-3 gap-5 p-11">
+        {drivers.map((driver) => (
+          <DriverPerMonthCard
+            key={driver.driver_id}
+            driver={driver}
+            setBookDriver={setBookDriver}
+          ></DriverPerMonthCard>
+        ))}
+      </div>
+      {bookDriver && <BookingDriver bookDriver={bookDriver}></BookingDriver>}
+    </div>
+  );
 };
 
 export default DriverPerMonth;
--- src/Components/Services/Driver/DriverPerMonthCard.js
+++ src/Components/Services/Driver/DriverPerMonthCard.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
 
-const DriverPerMonthCard = () => {
-    return (
-        <div>
-            
+const DriverPerMonthCard = ({ driver, setBookDriver }) => {
+  const { img, name, location, gender, salary } = driver;
+  return (
+    <div>
+      <div class="card w-96 bg-transparent border-2 shadow-xl transform transition-transform hover:scale-105 hover:bg-gradient-to-t from-secondary to-accent hover:shadow-lg">
+        <figure class="px-10 pt-7">
+          <img src={img} alt="driver" class="rounded-xl h-52 w-52" />
+        </figure>
+        <div class="card-body items-center text-center">
+          <h2 class="card-title text-purple-900 font-bold">{name}</h2>
+          <p>
+            <strong>Location :</strong> {location}
+          </p>
+          <p>
+            <strong>Gender :</strong> {gender}
+          </p>
+          <p>
+            <strong>Salary :</strong> {salary},
+          </p>
+          <div class="card-actions pt-5">
+            <label
+              for="booking-driver"
+              onClick={() => setBookDriver(driver)}
+              class="btn btn-sm border-purple-500 text-purple-800 text-xs font-bold bg-gradient-to-r from-primary from-10% via-secondary via-30% to-90% to-accent"
+            >
+              Know More
+            </label>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DriverPerMonthCard;
