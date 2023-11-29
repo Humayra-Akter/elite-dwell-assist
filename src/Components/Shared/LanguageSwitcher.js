6e1c1e98f2282d38baed1f700074d03998cb485d
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2023-11-29 23:06:26 +0600

    admin graph added

--- src/Components/Shared/LanguageSwitcher.js
+++ src/Components/Shared/LanguageSwitcher.js
@@ -3,20 +3,20 @@ import React, { useEffect } from "react";
 const LanguageSwitcher = () => {
   useEffect(() => {
     function googleTranslateElementInit() {
-      // new window.google.translate.TranslateElement(
-      //   {
-      //     pageLanguage: "en",
-      //     includedLanguages: "bn,en",
-      //     layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
-      //   },
-      //   "google_translate_element"
-      // );
+      new window.google.translate.TranslateElement(
+        {
+          pageLanguage: "en",
+          includedLanguages: "bn,en",
+          layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
+        },
+        "google_translate_element"
+      );
     }
-
     googleTranslateElementInit();
   }, []);
 
-  return <button id="google_translate_element"></button>;
+  // return <button id="google_translate_element"></button>;
+  return <div id="google_translate_element"></div>;
 };
 
 export default LanguageSwitcher;
